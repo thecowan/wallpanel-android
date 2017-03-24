28da23d9e3f4737c3ee036fbaa9002efe293df82
QuadPortNick <quadportnick@users.noreply.github.com>
2017-03-23 20:56:00 -0400

    Added feature to have HomeDash autostart when Android boots

---
+++ homeDashApp/src/main/java/de/rhuber/homedash/BootUpReceiver.java
@@ -0,0 +1,23 @@
+package de.rhuber.homedash;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class BootUpReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        final boolean startOnBoot = sharedPreferences.getBoolean(context.getResources().getString(R.string.key_setting_start_on_boot),false);
+
+        if (startOnBoot) {
+            Intent i = new Intent(context, SettingsActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+
+}
