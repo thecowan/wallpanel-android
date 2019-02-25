849575bd65708ce9e6965c0fcd39cd1f041e0a20
mex <allofmex@web.de>
2019-02-25 18:29:40 +0100

    Add screen wakeTime command attribute and wake=false handling

--- WallPanelApp/src/main/java/com/thanksmister/iot/wallpanel/utils/MqttUtils.kt
+++ WallPanelApp/src/main/java/com/thanksmister/iot/wallpanel/utils/MqttUtils.kt
@@ -48,6 +48,7 @@ class MqttUtils {
         const val COMMAND_CAMERA = "camera"
         const val COMMAND_RELAUNCH = "relaunch"
         const val COMMAND_WAKE = "wake"
+        const val COMMAND_WAKETIME = "wakeTime"
         const val COMMAND_BRIGHTNESS = "brightness"
         const val COMMAND_NOTIFICATION = "notification"
         const val COMMAND_RELOAD = "reload"
